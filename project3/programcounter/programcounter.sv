ac053983fbf838a0eb5c12dc836fb6b1562c3d39
thebrandontyler <brandontyler@me.com>
2013-11-18 20:41:21 -0800

    instruction register and updated variable names in program counter

--- project3/programcounter/programcounter.sv
+++ project3/programcounter/programcounter.sv
@@ -1,8 +1,8 @@
 module programcounter(input logic clk,
 							 input logic en,
-							 input logic [15:0]next_instr,
-							 output logic [15:0]curr_instr);
+							 input logic [15:0]next_addr,
+							 output logic [15:0]curr_addr);
 							 
-	flopen procount(clk, en, next_instr, curr_instr);
+	flopen procount(clk, en, next_addr, curr_addr);
 endmodule
 	
---
+++ project3/programcounter/programcounter.sv.bak
@@ -0,0 +1,8 @@
+module programcounter(input logic clk,
+							 input logic en,
+							 input logic [15:0]next_instr,
+							 output logic [15:0]curr_instr);
+							 
+	flopen procount(clk, en, next_instr, curr_instr);
+endmodule
+	
