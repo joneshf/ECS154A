a7d98f27de0ed7508bdc3089bdabd396fdf75dc7
thebrandontyler <brandontyler@me.com>
2013-11-18 21:39:35 -0800

    instruction register

--- project3/instructionregister/instructionregister.sv
+++ project3/instructionregister/instructionregister.sv
@@ -1,7 +1,7 @@
 module instructionregister(input logic clk,
-									input logic en,
-									input logic [15:0]next_instr,
-									output logic [15:0]curr_instr);
+									input logic re,  					//read enable
+									input logic [15:0]curr_addr,	//current address from program counter
+									output logic [15:0]curr_instr); //outputs current instruction
 							 
-	flopen instreg(clk, en, next_instr, curr_instr);
+	flopen instreg(clk, re, curr_addr, curr_instr);
 endmodule
