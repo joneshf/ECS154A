d8be2afb2f19557bf7c6471f8bef83ed7c0b58f5
thebrandontyler <brandontyler@me.com>
2013-11-18 20:08:49 -0800

    Flags Register Complete with Waveforms

--- project3/flagregister/flagregister.sv
+++ project3/flagregister/flagregister.sv
@@ -3,9 +3,17 @@ module flagregister(input logic clk,
 						  input logic [2:0]s,     //selector bits
 						  output logic [7:0]q);	  //output flag bits
 	always_ff@(posedge clk)
-	if (s[2])
+	if (s[2])			 //if MSB is set to 1, copy all input flags to output flags
 		begin
-			q = f;
+			q[0] = f[0]; //copy zero flag input to zero flag output
+			q[1] = f[1]; //copy negative flag input to neg flag output
+			q[2] = f[2]; //copy carry flag input to carry flag output
+			q[3] = f[3]; //copy overflow flag input to overflow flag output
+			q[4] = f[4]; //copy interrupt flag input to interrupt flag output
+			q[5] = f[5]; //copy always flag input to always flag output
+			q[6] = f[6]; //not used, but quartus gave a warning
+			q[7] = f[7]; //not used, but quartus gave a warning
+			
 		end
 	else
 		begin
