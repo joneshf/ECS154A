e7ed9b873a04365de297c8f139f2ccc460dec9f5
Hardy Jones <jones3.hardy@gmail.com>
2013-11-19 21:41:40 -0800

    Fixed demux.

--- project3/demux/demux.sv
+++ project3/demux/demux.sv
@@ -2,13 +2,19 @@ module demux(input logic [15:0] datain,
 				  input logic select,
 				  output logic [15:0] d0,
 				  output logic [15:0] d1);
-	   always_comb
-		if (select == 0){
-			d0 <= datain;
-			d1 <= 16'bz;
-		}
-		else if (select == 1){
-			d1 <= datain;
-			d0 <= 16'bz;
-		}
+	always_comb
+		begin
+			case (select)
+				1'b0:
+					begin
+						d0 = datain;
+						d1 = 16'bz;
+					end
+				1'b1:
+					begin
+						d0 = 16'bz;
+						d1 = datain;
+					end
+			endcase
+		end
 endmodule
