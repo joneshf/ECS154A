7ea091b70b327b7dde144cdcf82c0c1b056a0579
thebrandontyler <brandontyler@me.com>
2013-11-20 17:08:33 -0800

    working on cpu

--- project3/programcounter/programcounter.sv
+++ project3/programcounter/programcounter.sv
@@ -2,7 +2,11 @@ module programcounter(input logic clk,
 							 input logic en,
 							 input logic [15:0]next_addr,
 							 output logic [15:0]curr_addr);
-							 
-	flopen procount(clk, en, next_addr, curr_addr);
+	
+	logic [15:0] temp = 16'b0;
+	flopen procount(clk, en, temp, curr_addr);
+	
+	always_ff@(posedge en)
+	temp <= next_addr;
 endmodule
 	
