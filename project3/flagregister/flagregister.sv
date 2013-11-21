4721b84228995264385b50d34d7b3d51a0f41fd0
Hardy Jones <jones3.hardy@gmail.com>
2013-11-21 01:49:14 -0800

    Updated flag register for new logic.

--- project3/flagregister/flagregister.sv
+++ project3/flagregister/flagregister.sv
@@ -1,28 +1,62 @@
 module flagregister(input logic clk,
 						  input logic [7:0]f,	  //flag bits
-						  input logic [2:0]s,     //selector bits
+						  input logic [4:0]i,	  //instruction bits
+						  input logic [2:0]s,	  //flag selector bits
+						  input logic val,	  //LSB for LDIF/MOVF
 						  output logic [7:0]q);	  //output flag bits
 	always_ff@(posedge clk)
-	if (s[2])			 //if MSB is set to 1, copy all input flags to output flags
-		begin
-			q[0] = f[0]; //copy zero flag input to zero flag output
-			q[1] = f[1]; //copy negative flag input to neg flag output
-			q[2] = f[2]; //copy carry flag input to carry flag output
-			q[3] = f[3]; //copy overflow flag input to overflow flag output
-			q[4] = f[4]; //copy interrupt flag input to interrupt flag output
-			q[5] = f[5]; //copy always flag input to always flag output
-			q[6] = f[6]; //not used, but quartus gave a warning
-			q[7] = f[7]; //not used, but quartus gave a warning
-			
-		end
-	else
-		begin
-			case(s)
-				3'b000: q[0] = 1;  //Zero flag
-				3'b001: q[1] = 1;  //Negative flag
-				3'b010: q[2] = 1;  //Carry flag
-				3'b011: q[3] = 1;  //Overflow flag
+
+	case (i)
+		5'h00: q[3:0] = f[3:0]; // ADD  CNOZ
+		5'h01: q[3:0] = f[3:0]; // SUB  CNOZ
+		5'h02: q[3:0] = f[3:0]; // ADDI CNOZ
+		5'h03: begin 				// AND   N Z
+				q[2] = f[2];
+				q[0] = f[0];
+			end
+		5'h04: begin 				// OR    N Z
+				q[2] = f[2];
+				q[0] = f[0];
+			end
+		5'h05: begin 				// XOR   N Z
+				q[2] = f[2];
+				q[0] = f[0];
+			end
+		5'h06: begin 				// NOT   N Z
+				q[2] = f[2];
+				q[0] = f[0];
+			end
+		5'h07: begin 				// INV   N Z
+				q[2] = f[2];
+				q[0] = f[0];
+			end
+		5'h08: begin 				// ROR  CN Z
+				q[2:1] = f[2:1];
+				q[0] = f[0];
+			end
+		5'h09: begin 				// ROL  CN Z
+				q[2:1] = f[2:1];
+				q[0] = f[0];
+			end
+		5'h0A: begin 				// SHR  CN Z
+				q[2:1] = f[2:1];
+				q[0] = f[0];
+			end
+		5'h0B: begin 				// SHL  CN Z
+				q[2:1] = f[2:1];
+				q[0] = f[0];
+			end
+		5'h19:						// LDIF
+			case (s)
+				3'h0: q[0] = val;
+				3'h1: q[1] = val;
+				3'h2: q[2] = val;
+				3'h3: q[3] = val;
+				3'h4: q[4] = val;
+				3'h5: q[5] = val;
+				3'h6: q[6] = val;
 			endcase
-		end
-		
+		5'h1A: if(val == 1'b0) q = f; // MOVF
+	endcase
+
 endmodule
