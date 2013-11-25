213f0136412fa4ac7c3018672154b6dfabe2d1b9
Hardy Jones <jones3.hardy@gmail.com>
2013-11-25 08:01:24 -0800

    Got things connected.

--- project3/cpu/registers/registers.sv
+++ project3/cpu/registers/registers.sv
@@ -3,23 +3,24 @@ module registers(input logic clk,
 						input logic we, 											//write enable
 						input logic [15:0] d_input,							//16-bit bus input
 						output logic [15:0] a_output,							//16-bit bus output
-						output logic [15:0] b_output);						//16-bit bus output
+						output logic [15:0] b_output,							//16-bit bus output
+						output logic [15:0] r0, r1, r2, r3, r4, r5, r6, r7);	//16-bit debug registers
 
 	// These are used to set up the registers.
 	logic [15:0] tempin;
-	logic [7:0][15:0] tempout;
+	// logic [7:0][15:0] tempout;
 	logic [7:0] en = 8'b0;
 
 	// The internal registers of this block.
-	flopen R0(clk, en[0], tempin, tempout[0]);
-	flopen R1(clk, en[1], tempin, tempout[1]);
-	flopen R2(clk, en[2], tempin, tempout[2]);
-	flopen R3(clk, en[3], tempin, tempout[3]);
+	flopen R0(clk, en[0], tempin, r0);
+	flopen R1(clk, en[1], tempin, r1);
+	flopen R2(clk, en[2], tempin, r2);
+	flopen R3(clk, en[3], tempin, r3);
 
-	flopen R4(clk, en[4], tempin, tempout[4]);
-	flopen R5(clk, en[5], tempin, tempout[5]);
-	flopen R6(clk, en[6], tempin, tempout[6]);
-	flopen R7(clk, en[7], tempin, tempout[7]);
+	flopen R4(clk, en[4], tempin, r4);
+	flopen R5(clk, en[5], tempin, r5);
+	flopen R6(clk, en[6], tempin, r6);
+	flopen R7(clk, en[7], tempin, r7);
 
 	always_ff
 		begin
@@ -45,26 +46,26 @@ module registers(input logic clk,
 
 			// Select the a_output based on what a_index is.
 			case(a_index)
-				3'b000: a_output = tempout[0];
-				3'b001: a_output = tempout[1];
-				3'b010: a_output = tempout[2];
-				3'b011: a_output = tempout[3];
-				3'b100: a_output = tempout[4];
-				3'b101: a_output = tempout[5];
-				3'b110: a_output = tempout[6];
-				3'b111: a_output = tempout[7];
+				3'b000: a_output = r0;
+				3'b001: a_output = r1;
+				3'b010: a_output = r2;
+				3'b011: a_output = r3;
+				3'b100: a_output = r4;
+				3'b101: a_output = r5;
+				3'b110: a_output = r6;
+				3'b111: a_output = r7;
 			endcase
 
 			// Select the b_output based on what b_index is.
 			case(b_index)
-				3'b000: b_output = tempout[0];
-				3'b001: b_output = tempout[1];
-				3'b010: b_output = tempout[2];
-				3'b011: b_output = tempout[3];
-				3'b100: b_output = tempout[4];
-				3'b101: b_output = tempout[5];
-				3'b110: b_output = tempout[6];
-				3'b111: b_output = tempout[7];
+				3'b000: b_output = r0;
+				3'b001: b_output = r1;
+				3'b010: b_output = r2;
+				3'b011: b_output = r3;
+				3'b100: b_output = r4;
+				3'b101: b_output = r5;
+				3'b110: b_output = r6;
+				3'b111: b_output = r7;
 			endcase
 		end
 
