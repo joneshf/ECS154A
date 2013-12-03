c6b313bce62add5affef2f388dad1446c457032a
schavery <schavery@gmail.com>
2013-12-02 18:22:10 -0800

    registers hooked up

--- project4/uart.sv
+++ project4/uart.sv
@@ -9,37 +9,19 @@ module uart( // the cpu interface
 				 // the serial interface
 				input logic RX, // receive
 				output logic TX); // transmit
-				
-	/*
-		registers:
-		0 - status / command register
-			set by cpu through the data bus. But some conditions
-			within the module will cause status flags to change.
-	*/
-	
-		logic [7:0] statout;
-		statusregister sr(clk, DATA, statout);
-	
-	/*
-		1 - interrupt mask register
-			set by the cpu. these flags can be directly compared with 
-			the status flags, and the output will be interrupt out.
-	*/
-		logic [7:0] intout;
-		regoneinterrupt im(clk, DATA, intout);
-	/*
-		2 - data register
-			the data to send to the cpu or the data coming from cpu
 
-		3 - baud rate divisor register
-			the amount to divide the clock by, to hold the RX or TX 
-			for each bit, for this number of clocks.
-	*/
-	
-	/*
-		submodules:
-		crc - calculate the checksum of incoming or outgoing data
-		.... more?
-	*/
+		logic [7:0] statin, intin, dataregin, baudin;
+		logic [7:0] statout, intout, dataregout, baudout;
+		
+		//tristate ts(somekindofmuxofregouts, ~NO, DATA);
+		
+		onefourbusdecode busdmx(DATA, ADDR, ~NCS, statin, intin, dataregin, baudin);
+		
+		statusregister sr(CLK, statin, statout);
+		regoneinterrupt im(CLK, intin, intout);
+		regtwodata datareg(CLK, dataregin, dataregout);
+		baudratedivisor brd(CLK, baudin, baudout);
+		
+		interruptlogic il(statout, intout, NINT);
 		
 endmodule
